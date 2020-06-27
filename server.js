8139e242e68a4f0cfc6dbb41c25cae64d1179fea
Josh Boyd <joshboyd@joshs-mbp.myfiosgateway.com>
2020-06-27 14:02:22 -0400

    hopefully works

--- server.js
+++ server.js
@@ -22,11 +22,16 @@ app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
 
-mongoose.connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI, {
+  useNewParser: true,
+  useFindAndModify: false
+}) 
 
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+// app.listen(PORT, () => {
+//   console.log(`App running on port ${PORT}!`);
+// });
+
+app.listen(process.env.PORT, '0.0.0.0');
